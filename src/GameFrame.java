5a2231c078e8a241ab7028c4a3447493f7541d83
Rajan Marasini <official.rajan2060@gmail.com>
2024-03-02 22:25:04 +0545

    Initial commit

---
+++ src/GameFrame.java
@@ -0,0 +1,21 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+
+    GamePanel panel;
+
+    GameFrame() {
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("PingPong");
+        this.setResizable(false);
+        this.setBackground(Color.BLACK);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+}
